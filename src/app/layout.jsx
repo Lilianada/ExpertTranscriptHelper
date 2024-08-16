d1361ee30935bd8c3fc6ffc3a61ea94fd3a1a761
Lilianada <lilibest30.lb@gmail.com>
2024-08-16 14:39:32 +0100

    added favicons and site manifest

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
   title: 'ExpertTranscriptHelper | Earn UoPeople Credits',
   description:
     'Let us take your Sophia Learning and Coursera courses for you. ExpertTranscriptHelper provides a streamlined process for busy adults.',
-    keywords: 'transcript help, UoPeople credits, online learning assistance',
+  keywords: 'transcript help, UoPeople credits, online learning assistance',
 }
 
 export default function RootLayout({ children }) {
@@ -26,13 +26,31 @@ export default function RootLayout({ children }) {
       )}
     >
       <head>
-        <title>{metadata.title}</title> 
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>{metadata.title}</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta property="og:url" content="https://expertTranscriptHelper.com" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="src/app/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="preconnect"
           href="https://cdn.fontshare.com"
@@ -42,7 +60,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@800,500,700&display=swap"
         />
-        </head>
+      </head>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   )
